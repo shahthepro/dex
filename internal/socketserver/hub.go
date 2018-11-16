c21e7c74f058ac39e38895c5de6fd3fccd4c9717
Shahul Hameed <shahshah1995@gmail.com>
2018-11-16 22:25:13 +0530

    Update

--- internal/socketserver/hub.go
+++ internal/socketserver/hub.go
@@ -1,8 +1,10 @@
 package socketserver
 
+import "github.com/gorilla/websocket"
+
 // Hub is a collection of clients of similar interests
 type Hub struct {
-	clients    map[*Client]bool
+	clients    []*websocket.Conn
 	broadcast  chan []byte
 	register   chan *Client
 	unregister chan *Client
@@ -11,7 +13,7 @@ type Hub struct {
 // NewHub creates and returns new instance of hub
 func NewHub() *Hub {
 	return &Hub{
-		clients:    make(map[*Client]bool),
+		clients:    []*websocket.Conn{},
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
--- internal/socketserver/main.go
+++ internal/socketserver/main.go
@@ -1,9 +1,15 @@
 package socketserver
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/gorilla/mux"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,24 +24,39 @@ type SocketServer struct {
 // Start starts websocket server
 func (socketServer *SocketServer) Start() {
 
-	var upgrader = websocket.Upgrader{
+	upgrader := websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
-
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		// fmt.Println()
 		return r.Header.Get("Origin") == socketServer.webappHost
 	}
-
 	upgrader.EnableCompression = true
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+	router.HandleFunc("/ws/{token:0x[0-9A-Za-z]{40}}/{base:0x[0-9A-Za-z]{40}}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
 		header := w.Header()
 		header.Set("Content-Security-Policy", fmt.Sprintf("content-src: '%s'", socketServer.webappHost))
+
+		trimmedToken := strings.TrimPrefix(vars["token"], "0x")
+		trimmedBase := strings.TrimPrefix(vars["base"], "0x")
+
+		token := common.HexToAddress(trimmedToken)
+		base := common.HexToAddress(trimmedBase)
+
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			log.Println("CONN", err)
+		}
+
+		if bytes.Compare(token.Bytes(), base.Bytes()) >= 0 {
+			_ = conn.WriteMessage(1, []byte("{\"error\":\"Invalid token pair\"}"))
+			conn.Close()
+			return
 		}
 
 		for {
@@ -54,6 +75,32 @@ func (socketServer *SocketServer) Start() {
 			}
 		}
 	})
+
+	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	header := w.Header()
+	// 	header.Set("Content-Security-Policy", fmt.Sprintf("content-src: '%s'", socketServer.webappHost))
+	// 	conn, err := upgrader.Upgrade(w, r, nil)
+	// 	if err != nil {
+	// 		log.Println("CONN", err)
+	// 	}
+
+	// 	for {
+	// 		// Read message from browser
+	// 		msgType, msg, err := conn.ReadMessage()
+	// 		if err != nil {
+	// 			return
+	// 		}
+
+	// 		// Print the message to the console
+	// 		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+	// 		// Write message back to browser
+	// 		if err = conn.WriteMessage(msgType, msg); err != nil {
+	// 			return
+	// 		}
+	// 	}
+	// })
+	socketServer.server.Handler = router
 
 	go func() {
 		if err := socketServer.server.ListenAndServe(); err != nil {
