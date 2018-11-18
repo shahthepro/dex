f610b3c53f5eb391784d5f53de9469e16c51a997
Shahul Hameed <shahshah1995@gmail.com>
2018-11-18 17:45:22 +0530

    Commit before mess up

--- internal/socketserver/hub.go
+++ internal/socketserver/hub.go
@@ -1,21 +1,61 @@
 package socketserver
 
-import "github.com/gorilla/websocket"
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait = 5 * time.Second
+	pongWait  = 2 * time.Second
+)
 
 // Hub is a collection of clients of similar interests
 type Hub struct {
-	clients    []*websocket.Conn
+	clients    map[*websocket.Conn]bool
 	broadcast  chan []byte
-	register   chan *Client
-	unregister chan *Client
+	register   chan *websocket.Conn
+	unregister chan *websocket.Conn
+	done       chan bool
+}
+
+// Run hub
+func (hub *Hub) Run() {
+	for {
+		select {
+		case client := <-hub.register:
+			hub.clients[client] = true
+		case client := <-hub.register:
+			delete(hub.clients, client)
+		case message := <-hub.broadcast:
+			for client := range hub.clients {
+				go hub.sendMessageToClient(client, &message)
+			}
+		}
+	}
+}
+
+func (hub *Hub) sendMessageToClient(client *websocket.Conn, message *[]byte) {
+	client.SetWriteDeadline(time.Now().Add(writeWait))
+
+	writer, err := client.NextWriter(websocket.TextMessage)
+	if err != nil {
+		hub.unregister <- client
+		return
+	}
+
+	if _, err := writer.Write(*message); err != nil {
+		hub.unregister <- client
+	}
 }
 
 // NewHub creates and returns new instance of hub
 func NewHub() *Hub {
 	return &Hub{
-		clients:    []*websocket.Conn{},
+		clients:    map[*websocket.Conn]bool{},
 		broadcast:  make(chan []byte),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
+		register:   make(chan *websocket.Conn),
+		unregister: make(chan *websocket.Conn),
 	}
 }
