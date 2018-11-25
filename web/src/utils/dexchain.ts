8beda845fd2fcec2781fe20ef007abd4af27f95f
Shahul Hameed <shahshah1995@gmail.com>
2018-11-25 21:57:52 +0530

    DEXChain contract util

---
+++ web/src/utils/dexchain.ts
@@ -0,0 +1,37 @@
+import store from '@/store'
+import BLOCKCHAIN_INFO from '@/core/blockchain'
+import TOKENS from '@/core/tokens'
+
+async function withdrawTokens(tokenSymbol: string, amount: string) {
+  const { isConnected, current: wallet } = store.getters.wallet
+
+  if (!isConnected) {
+    throw new Error('You have not connected a wallet yet. Connect a wallet and then try again.')
+  }
+
+  const EXCHANGE_ABI = BLOCKCHAIN_INFO.getExchangeABI()
+  const EXCHANGE_CONTRACT_ADDRESS = BLOCKCHAIN_INFO.getExchangeContractInfo().address
+
+  const web3 = wallet.web3()
+
+  const token = TOKENS.getBySymbol(tokenSymbol)
+
+  let unitAmount = TOKENS.convertFixedToBigInt(amount, token.decimal)
+
+  const DEXChainContract = new web3.eth.Contract(EXCHANGE_ABI, EXCHANGE_CONTRACT_ADDRESS)
+
+  const data = DEXChainContract.methods.transferHomeViaRelay(
+    token,
+    unitAmount
+  ).encodeABI()
+
+  return wallet.sendTx({
+    to: EXCHANGE_CONTRACT_ADDRESS,
+    value: 0,
+    gas: 500000,
+    gasPrice: 0,
+    data
+  })
+}
+
+export default { withdrawTokens }
